4ac8a59cddd8536f35f7272367a726f3c8f6fbb8
JaViKo500 <javikogutierrez64@gmail.com>
2025-03-29 23:25:40 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { constAndLet } from './basics/constAndLet'
+
+function App() {
+  constAndLet();
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
